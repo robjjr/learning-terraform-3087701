a287cf35f72654836e7dc7725dc55e517d9cb590
robjjr <rjusayjr@gmail.com>
2024-12-09 11:16:43 -0500

    Create Sec Group 3

--- main.tf
+++ main.tf
@@ -47,10 +47,10 @@ resource "aws_security_group_rule" "blog_http_in"{
   aws_security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_http_in"{
+resource "aws_security_group_rule" "blog_https_in"{
   type        = "ingress"
   from_port   = 443
-  to_port     = 43
+  to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
