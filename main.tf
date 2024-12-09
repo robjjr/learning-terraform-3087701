80af8e37f4ea4d20fbb0a06fe3130a17931c59ca
robjjr <rjusayjr@gmail.com>
2024-12-09 11:17:36 -0500

    Create Sec Group 4

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group_rule" "blog_https_in"{
 resource "aws_security_group_rule" "blog_everything_out"{
   type        = "engress"
   from_port   = 0
-  to_port     = 
+  to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
