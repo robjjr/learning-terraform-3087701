b257b042b2d137a8ebfa010ffed73f7e618a7cbc
robjjr <rjusayjr@gmail.com>
2024-12-10 11:32:51 -0500

    autoscale 10

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "autoscaling" {
 
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
+  
 
   name            = "blog-alb"
   vpc_id          = module.blog_vpc.vpc_id
