e08ee2f043fb05f57842cabe25ffda74b476cc31
robjjr <rjusayjr@gmail.com>
2024-12-10 11:34:00 -0500

    autoscale 11

--- main.tf
+++ main.tf
@@ -34,20 +34,19 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
-  
-  name = "blog"
-  min_size = 1
-  max_size = 2
 
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
-  security_groups = [module.blog_sg.security_group_id]
-
-  image_id          = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
